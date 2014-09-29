424798efcf17935275fc24386323af2a2f2d3324
Meddy B. <o_be_one@r0x.fr>
2014-09-29 09:25:02 -0400

    initiation

---
+++ mep-git.sh
@@ -0,0 +1,264 @@
+#! /bin/bash
+
+#########
+# Author : Meddy "o_be_one" Brai
+# Creation date : 31/07/2014
+#
+# Goal : Deploy or update main configurations of a server running Linux Ubuntu or Debian.
+# Syntax : ./mep-git.sh
+#########
+
+# Please edit following :
+
+GITREPO="github.com/o-be-one/git-deploy-linux.git" # github repositery to use
+TMPFOLDER="/tmp/mep-git" # temp folder wheres to work
+
+######
+# Edit who's following at your own risks !
+######
+
+DESTSCRIPT=`dirname $0`
+SCRIPT=`basename $0`
+. /lib/lsb/init-functions
+
+# Is the script launched as root user ?
+if [[ $EUID -ne 0 ]];
+then
+
+    log_daemon_msg "Please use this script as root." 1>&2
+    log_end_msg 1
+    echo
+    exit 1
+
+fi
+
+clear
+echo -e "Warning ! This script is for Debian and Ubuntu."
+echo -e "Do you want to continue ? [y/N]"
+read GOGO
+echo
+
+if [[ $GOGO =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+
+    # What to do ? You can use 0 to 9, a to z and A to Z.
+    echo -e "Que souhaitez-vous installer ou mettre à jour ? [all]"
+    echo -e "1 - .bashrc\n2 - .vimrc\n3 - atop\n4 - Login banner\n5 - Bash issue fix\nall - Do all"
+    read UPDT
+    echo
+   
+    # If nothing or all
+    [[ -z $UPDT ]] || [ $UPDT == "all" ] && UPDT="0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+   
+   
+    # We have 2 version of .vimrc. Which one use ?
+    if [[ $UPDT == *2* ]]
+    then
+   
+        echo -e "What you'll do on this server ? [1]"
+        echo -e "1 - .vimrc normal\n2 - .vimrc python"
+        read VIMRC
+      
+    fi
+   
+    # If temp folder doesn't exit, make parent dir.
+   
+    if [ ! -d $TMPFOLDER ]
+    then
+
+        mkdir -p $(dirname $TMPFOLDER)
+    
+    else
+
+        # Delete the temp folder if it already exists.
+        echo -e "\nThis folder already exists. Delete $TMPFOLDER ? [y/N]"
+        read GOGO
+
+        if [[ $GOGO =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+
+            rm -rf $TMPFOLDER
+
+        else
+
+            echo
+            log_daemon_msg "Maybe you need to edit this script ..."
+            log_end_msg 1
+            echo
+            exit 1
+
+        fi
+
+    fi
+
+    echo
+    
+    # Install git needed ?
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' git-core|grep "install ok installed")
+    if [ "" == "$PKG_OK" ]; then
+        log_warning_msg "git isn't installed. Starting installation ..."
+        sudo apt-get --force-yes --yes install git-core
+    fi
+   
+    # Clone the repositery
+    until [[ ! `git clone https://$GITREPO $TMPFOLDER 2>&1` == *failed* ]]
+    do
+   
+        echo
+        log_warning_msg "Login failed ... Please try again."
+        echo
+
+    done
+   
+        echo
+   
+    # Check updates
+    diff --brief <(sort $TMPFOLDER/mep-git.sh) <(sort $DESTSCRIPT/$SCRIPT) >/dev/null
+    comp_value=$?
+ 
+    if [ $comp_value -eq 1 ]
+    then
+
+        echo -e "An update of this script is available. Update it ? [Y/n]"
+        read GOGO
+       
+        if [[ ! $GOGO =~ ^([nN][oO]|[nN])$ ]]
+        then
+      
+            cp $TMPFOLDER/mep-git.sh $DESTSCRIPT/$SCRIPT
+            echo -e "\nUpdate finished. Can we delete folder $TMPFOLDER ? [Y/n]"
+            read GOGO
+
+            [[ ! $GOGO =~ ^([nN][oO]|[nN])$ ]] && rm -rf $TMPFOLDER
+   
+            $DESTSCRIPT/$SCRIPT && exit 0
+            
+      fi
+      
+   fi
+   
+    ##### Where to deploy files ?
+    ## .bashrc
+   if [[ $UPDT == *1* ]]; then
+      cp $TMPFOLDER/.bashrc /etc/skel
+      cp $TMPFOLDER/.bashrc /root/
+      for i in `ls /home/`; do cp $TMPFOLDER/.bashrc /home/$i; done
+      source ~/.bashrc
+      log_daemon_msg "bashrc update"
+      log_end_msg 0
+   fi
+
+    ## .vimrc
+   if [[ $UPDT == *2* ]]; then
+      
+      # Is vim installed ? Else install it !
+      PKG_OK=$(dpkg-query -W --showformat='${Status}\n' vim|grep "install ok installed")
+      if [ "" == "$PKG_OK" ]; then
+         log_warning_msg "vim is not installed. Install is starting ..."
+         sudo apt-get --force-yes --yes install vim
+      fi
+      
+      if [[ $VIMRC == "2" ]]
+      then
+      
+         apt-get --force-yes --yes install git pep8 exuberant-ctags
+         easy_install -U pytest
+         rm -rf ~/.vim
+         git clone git://github.com/wjeanneau/dotvim.git ~/.vim
+         rm -f ~/.vimrc
+         ln -s ~/.vim/vimrc ~/.vimrc
+         cp -rf ~/.vim /etc/skel
+         cp /etc/skel/.vim/vimrc /etc/skel/.vimrc
+         for i in `ls /home/`; do cp -rf ~/.vim /home/$i; done
+         cd ~/.vim
+         git submodule init
+         git submodule update
+         git submodule foreach git submodule init
+         git submodule foreach git submodule update
+         cd /etc/skel/.vim
+         git submodule init
+         git submodule update
+         git submodule foreach git submodule init
+         git submodule foreach git submodule update
+         
+         for i in `ls /home/`
+         do
+         
+            cd /home/$i/.vim
+            git submodule init
+            git submodule update
+            git submodule foreach git submodule init
+            git submodule foreach git submodule update
+            
+         done
+         
+         cd $DESTSCRIPT
+         
+      else
+      
+         cp $TMPFOLDER/.vimrc /etc/skel
+         cp $TMPFOLDER/.vimrc /root/
+         for i in `ls /home/`; do cp $TMPFOLDER/.vimrc /home/$i; done
+
+      fi
+      
+      log_daemon_msg "vimrc update"
+      log_end_msg 0
+   fi
+
+   ## atop
+   # is atop installed ? If not, install it.
+   if [[ $UPDT == *3* ]]; then
+      PKG_OK=$(dpkg-query -W --showformat='${Status}\n' atop|grep "install ok installed")
+      if [ "" == "$PKG_OK" ]; then
+         log_warning_msg "atop is not installed. Install is starting ..."
+         sudo apt-get --force-yes --yes install atop
+      fi
+      cp $TMPFOLDER/atop /etc/logrotate.d/atop
+      sed -i -e 's/600/300/g' /etc/init.d/atop
+      log_daemon_msg "Installation and/or configuration ATOP"
+      log_end_msg 0
+   fi
+    
+   ## login banner
+   if [[ $UPDT == *4* ]]; then
+      cp $TMPFOLDER/issue.net /etc/issue.net
+      sed -i -e 's/#Banner/Banner/g' /etc/ssh/sshd_config
+      log_daemon_msg "Login banner update"
+      log_end_msg 0
+   fi
+   
+   ## Bash issue fix
+   if [[ $UPDT == *5* ]]; then
+      
+      if [[ `env x='() { :;}; echo vulnerable' bash -c "echo this is a test" 2>/dev/null` == *vulnerable* ]]
+      then
+         apt-get --force-yes --yes install bash
+         log_daemon_msg "Bash issue fix"
+         log_end_msg 0
+      else
+         log_daemon_msg "Bash issue is already fixed"
+         log_end_msg 0
+      fi
+   
+   fi
+   
+   #####
+
+   echo -e "\nSetup complete. Can we delete the temp folder $TMPFOLDER ? [Y/n]"
+   read GOGO
+
+   [[ ! $GOGO =~ ^([nN][oO]|[nN])$ ]] && rm -rf $TMPFOLDER
+   
+   log_daemon_msg "System is deployed. See ya !"
+   log_end_msg 0
+   echo
+   exit 0
+
+else
+
+   log_warning_msg "Nothing changed. Bye !"
+   echo
+   exit 0
+
+fi
