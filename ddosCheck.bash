267997ac6ec45ab4ba5cca2748d80848e86b3478
Meddy B <o_be_one@r0x.fr>
2015-11-12 12:20:01 -0500

    Simplified config, removed MAXLAT
    Now the script considers that latency more than $LAT is a timeout, its easier to understand and configure the script.

--- ddosCheck.bash
+++ ddosCheck.bash
@@ -2,25 +2,24 @@
 
 ############
 # License : MIT
-# Author : Meddy "o_be_one" Brai for OVH (T : @o_b)
-# Version : 0.1
-# Date : 2015-11-10
+# Author : Meddy "o_be_one" Brai for OVH Anti-DDoS team (T : @o_b)
+# Version : 0.2
+# Date : 2015-11-12
 # Goal : start a TCPDump if there is a DDoS
 # HowTo : use it as root, edit vars, set cronjob
 # Compatibility : Debian & Ubuntu ; not tested on others
 ############
 
-### Setup (you can let it by default
+### Setup (you can let it by default)
 FILENAME="capture-ovh" # name of the file prefix. Suffix will be, by default, .DATE (here capture-ovh.2015-11-10)
-TCPFOLDER="/home/meddy/ddos-attempt" # where to store TCPDump files
-LASTTIME=10 # set time in minute after the last tcpdump
-TRYSITE="google.com duckduckgo.com r0x.fr" # domains to try
-COUNT=3 # number of pings to check each TRYSITE
-LAT=200 # max latency before considering
-MAXFAIL=2 # number of fail before considering DDoS (regarding number of TRYSITE) ; here 2 timeout is enough to start tcpdump
-MAXLAT=2 # number of more than LAT before considering DDoS (regarding number of TRYSITE) ; here 2 higher latency than LAT is enough to start tcpdump
+TCPFOLDER="/home/o_be_one/ddos-attempt" # where to store TCPDump files
+LASTTIME=10 # set time in minute to tcpdump after the last tcpdump
+TRYSITE="google.com duckduckgo.com r0x.fr ovh.com" # domains to try (more you add, more time it takes to check ...)
+COUNT=3 # number of pings to check each TRYSITE (useful to get relevant ping average)
+LAT=200 # max latency before considering timeout
+MAXFAIL=3 # number of fails before tcpdump (max number is the number of defined TRYSITE, by defaut 4)
 DATE=$(date +'%y-%m-%d_%H-%M-%S') # date format for saved files
-TCPCMD="tcpdump -w $TCPFOLDER/$FILENAME.$DATE -c 10 port not ssh" # command to TCPDump (dont forget to keep $TCPFOLDER/$FILENAME.$DATE)
+TCPCMD="tcpdump -w $TCPFOLDER/$FILENAME.$DATE -c 100000 port not ssh" # command to TCPDump (dont forget to keep $TCPFOLDER/$FILENAME.$DATE)
 
 ### Script, edit it at your own risks
 # load init functions
@@ -28,7 +27,6 @@ TCPCMD="tcpdump -w $TCPFOLDER/$FILENAME.$DATE -c 10 port not ssh" # command to T
 
 # init vars
 failcount=0
-latcount=0
 
 # check if root
 if [[ $EUID -ne 0 ]]; then
@@ -57,11 +55,9 @@ do
                 let failcount++
         # if no timeout, check latency
         elif [ $avglat -gt $LAT ]; then
-                let latcount++
+                let failcount++
         fi
 done
 
-# start TCPCMD if we have more timeout than MAXFAIL or more lantecys than MAXLAT (depending on LAT)
-if [ $failcount -ge $MAXFAIL -o $latcount -ge $MAXLAT ]; then
-        $TCPCMD
-fi
+# start TCPCMD if we have more timeout than MAXFAIL
+[[ $failcount -ge $MAXFAIL ]] && $TCPCMD
