1aaedacb97a896b27a2b6e9164e26ff592ff6604
Meddy B <o_be_one@r0x.fr>
2016-02-26 11:17:08 -0500

    Enhanced message for mails + minor fix

--- mysql-backup.bash
+++ mysql-backup.bash
@@ -12,19 +12,22 @@
 
 DATE=$(date +'%y-%m-%d') # date format for saved files
 BACKUPFOLDER="/backups/mysql" # where to keep backups ?
-DBTOBACKUP="awesomedb mybestdb" # name of db to backup (separated by space)
-MAXDAYS="15" # how mutch days we have to keep backups ?
+DBTOBACKUP="awesomedb mybestdb" # name of dbs to backup (separated by space)
+MAXDAYS="14" # how mutch days we have to keep backups ?
 PASSWD="MySQL_P@sSW0rD" # mysql password with read access to db you want to backup
 
 ## Edit following at your own risk /!\
 
-echo "Backup in progress ..."
+echo "MySQL backup in progress ..."
+
+# fix $MAXDAYS number to fit with find
+MAXDAYS=$(($MAXDAYS -1))
 
 # loop to do all dbs
 for i in $DBTOBACKUP; do
     echo
     echo "======================="
-    echo ">> Doing $i ..."
+    echo ">> Doing $BACKUPFOLDER/$i/${i}_$DATE.sql ..."
     
     # make folder (no problem if it exists), dump db, compess db
     /bin/mkdir -p $BACKUPFOLDER/$i
@@ -32,9 +35,10 @@ for i in $DBTOBACKUP; do
     /bin/gzip $BACKUPFOLDER/$i/${i}_$DATE.sql
 
     # delete all found files after $MAXDAY
+    echo "# Deleting : "
+    /usr/bin/find $BACKUPFOLDER/$i -type f -mtime +$MAXDAYS
     /usr/bin/find $BACKUPFOLDER/$i -type f -mtime +$MAXDAYS -delete
 
-    echo ">> Done in $BACKUPFOLDER/$i/${i}_$DATE.sql.gz"
     echo "======================="
 done
 
