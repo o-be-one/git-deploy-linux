3c654e28006cac9701b54099324df77111014b86
Meddy B <o_be_one@r0x.fr>
2016-03-30 09:33:10 -0400

    Update whitelist.bash

--- whitelist.bash
+++ whitelist.bash
@@ -17,17 +17,11 @@ iptables -P INPUT DROP
 # Allow existing connections to continue
 iptables -A INPUT -i venet0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Accept everything from defined remote ip
-#iptables -A INPUT -i venet0 -s 8.33.137.29 -j ACCEPT
-#iptables -A INPUT -i venet0 -s 62.4.23.242 -j ACCEPT
-
 inip() {
     iptables -A INPUT -i venet0 -s $1 -j ACCEPT
 }
 
 # Allow this host to connect defined remote ip
-#iptables -A OUTPUT -o venet0 -d 62.4.23.242 -m state --state ESTABLISHED,RELATED -j ACCEPT # example to allow this server to only answer to requests from defined ip
-#iptables -A OUTPUT -o venet0 -d 8.33.137.29 -j ACCEPT # allow this server to send requests to defined ip
 
 outip() { # allow this server to send requests to defined ip
     iptables -A OUTPUT -o venet0 -d $1 -j ACCEPT
